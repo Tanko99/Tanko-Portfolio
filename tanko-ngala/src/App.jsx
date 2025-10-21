34012cb16ae0710b209b42dd516d3765fa22c8c8
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-21 06:12:14 +0100

    setting up react and tailwind css

---
+++ tanko-ngala/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+
+function App(){
+  return (
+    <div>
+      <hi>Hello World!</hi>
+    </div>
+  );
+}
+
+export default App;
