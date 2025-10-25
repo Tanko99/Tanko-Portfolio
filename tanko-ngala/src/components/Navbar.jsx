779d348c131f3e0233be17af2875f9580b46cad2
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-25 11:47:40 +0100

    navbar: background color changed

--- tanko-ngala/src/components/Navbar.jsx
+++ tanko-ngala/src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
 import logo from '../assets/logo.png';
+import background from '../assets/bg-pic.jpg';
 
 function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
-        <nav className="flex md:flex-row flex-col w-full items-center justify-between px-6 sm:px-2 py-4 mb-40 bg-cyan-400 text-gray-800 font-poppins">
+        <nav
+        style={{ backgroundImage: `url(${background})`}} className="
+        bg-cover bg-center bg-no-repeat flex md:flex-row flex-col w-full items-center justify-between px-6 sm:px-2 py-4 mb-40 text-white font-poppins">
             <img src={logo} alt="my logo is here" className="rounded-full w-32 h-32" />
             <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
                 {isOpen? <X size={30} /> : <Menu size={30} /> }
@@ -16,11 +19,11 @@ function Navbar(){
             <ul  className={`z-50 md:flex md:items-center md:gap-6 absolute md:static left-0 w-full md:w-auto bg-black md:bg-transparent transition-all duration-300 ease-in-out ${
           isOpen ? "top-16 opacity-100" : "top-[-400px] opacity-0 md:opacity-100"
         }`} >
-            <li><Link to="/" className="hover:bg-purple-300 rounded-lg font-bold text-lg px-2 py-2 bg-cyan-400">Home</Link></li>
-            <li><Link to="about" className="hover:bg-purple-300 rounded-lg font-bold text-lg px-2 py-2 bg-cyan-400">About Me</Link></li>
-            <li><Link to="/projects" className="hover:bg-purple-300 rounded-lg font-bold text-lg px-2 py-2 bg-cyan-400">Projects</Link></li>
-            <li><Link to="/skills" className="hover:bg-purple-300 rounded-lg font-bold text-lg px-2 py-2 bg-cyan-400">Skills</Link></li>
-            <li><Link to="/contact" className="hover:bg-purple-300 rounded-lg font-bold text-lg px-2 py-2 bg-cyan-400">Contact Me</Link></li>
+            <li><Link to="/" className="hover:bg-purple-500 rounded-lg font-bold text-xl px-2 py-2 bg-purple-800">Home</Link></li>
+            <li><Link to="about" className="hover:bg-purple-500 rounded-lg font-bold text-xl px-4 py-2 bg-purple-800">About Me</Link></li>
+            <li><Link to="/projects" className="hover:bg-purple-500 rounded-lg font-bold text-xl px-4 py-2 bg-purple-800">Projects</Link></li>
+            <li><Link to="/skills" className="hover:bg-purple-500 rounded-lg font-bold text-lg px-4 py-2 bg-purple-800">Skills</Link></li>
+            <li><Link to="/contact" className="hover:bg-purple-500 rounded-lg font-bold text-xl px-4 py-2 bg-purple-800">Contact Me</Link></li>
         </ul>
  </nav>
     );
