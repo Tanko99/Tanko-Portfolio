336aa2b087902eeb5b1e4615cde9a2d8a51708f0
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-25 10:19:59 +0100

    Homepage: baxkkgound color and button color change

--- tanko-ngala/src/pages/HomePage.jsx
+++ tanko-ngala/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom"
 import { Mail } from "lucide-react"
 import picture from '../assets/picre.jpg';
-import background from '../assets/background-image.jpg';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
@@ -9,7 +8,7 @@ function Home(){
     
     return(
         <div
-         style={{ backgroundImage: `url(${background})` }} className="font-poppins, text-black bg-cover bg-no-repeat bg-center w-full min-h-screen ">
+          className="font-poppins, text-gray-200 bg-black w-full min-h-screen ">
             <Navbar />
                <div className=" gap-x-8 flex flex-col md:flex-row items-center justify-center  items-center w-full">
                    <div className="max-w-md w-full md:w-1/2 h-auto mx-2 mt-8">
@@ -21,8 +20,8 @@ function Home(){
                          <em> a frontend web developer and a cloud computing enthusiast, passionate
                         about crafting interactive websites and seamless digital experiences!</em>
                 </p>
-                <Link to="/projects" className="flex items-center text-lg font-bold px-4 py-2 mb-6 bg-cyan-600 rounded-3xl transition ">View my work</Link>
-                <Link to="/contact" className="flex items-center text-lg mb-20  font-bold  gap-2 px-4 py-2 bg-cyan-600 rounded-3xl transition hover:bg-purple-8000">
+                <Link to="/projects" className="flex items-center text-lg font-bold px-4 py-2 mb-6 bg-purple-700 rounded-3xl transition ">View my work</Link>
+                <Link to="/contact" className="flex items-center text-lg mb-20  font-bold  gap-2 px-4 py-2 bg-purple-700 rounded-3xl transition hover:bg-purple-8000">
                 <Mail className="w-8 h-8"/>
                 Contact Me
                 </Link>
