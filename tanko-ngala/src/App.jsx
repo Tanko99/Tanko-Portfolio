42b09110fdc77f100ab552f20830ca4187a12c2f
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-25 08:47:26 +0100

    about page fixed and responsiveness ensured

--- tanko-ngala/src/App.jsx
+++ tanko-ngala/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import Home from "./pages/HomePage";
+import AboutMe from "./pages/AboutPage";
 
 function App(){
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
       </Routes>
     </Router>
   );
