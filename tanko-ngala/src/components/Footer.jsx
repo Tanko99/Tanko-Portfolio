184b463069c52b744acc05422fc5602a52cdc323
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-25 11:26:21 +0100

    footer: backgound color changed

--- tanko-ngala/src/components/Footer.jsx
+++ tanko-ngala/src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import { Facebook, Linkedin, Twitter, Github } from "lucide-react";
 import logo from '../assets/logo.png';
+import background from '../assets/bg-pic.jpg';
 
 function Footer(){
     return (
-        <footer className="bg-black mt-16 p-8 font-poppins flex md:flex-row flex-col items-center justify-between" >
-            <img src={logo} alt="my personal logo"  className="w-16 h-16 rounded-full" />
-            <p className="text-sm text-gray-200 md:text-xl sm:text-lg text-center md:text-right">
+        <footer style={{backgroundImage: `url(${background})`}} className="bg-cover
+        bg-center bg-no-repeat  mt-16 p-8 font-poppins text=yellow-200 flex md:flex-row flex-col items-center justify-between" >
+            <img src={logo} alt="my personal logo"  className="w-18 h-18 rounded-full" />
+            <p className="text-sm  md:text-xl sm:text-lg text-center md:text-right">
                 &copy; {new Date().getFullYear()} <em>Tanko Fabrice. All rights reserved.</em>
              </p>
             <div className="flex text-white  gap-2">
