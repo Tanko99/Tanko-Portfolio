383d3f385d938a7cf6edd8ba207fa6a8f6f4e077
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-28 11:42:58 +0100

    conatct added to APP

--- tanko-ngala/src/App.jsx
+++ tanko-ngala/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import Home from "./pages/HomePage";
 import AboutMe from "./pages/AboutPage";
 import Skills from "./pages/SkillsPage";
+import ConactMe from '../pages/ContactPage';
 
 function App(){
   return (
@@ -10,6 +11,7 @@ function App(){
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutMe />} />
         <Route path="/skills"  element={<Skills />} />
+        <Route path="/contact" element={<ContactMe />} />
       </Routes>
     </Router>
   );
