127c6f5ca44e33df0b542f172d2c936abc692301
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-29 18:44:47 +0100

    footer; styled and x(twitter) i con added

--- tanko-ngala/src/components/Footer.jsx
+++ tanko-ngala/src/components/Footer.jsx
@@ -1,21 +1,24 @@
-import { Facebook, Linkedin, Twitter, Github } from "lucide-react";
+import { Facebook, Linkedin,  Github } from "lucide-react";
 import logo from '../assets/logo.png';
 import background from '../assets/bg-pic.jpg';
+import icon from '../assets/x-icon.png';
+
 
 function Footer(){
     return (
         <footer style={{backgroundImage: `url(${background})`}} className="bg-cover
-        bg-center bg-no-repeat  mt-16 p-8 font-poppins text=yellow-200 flex md:flex-row flex-col items-center justify-between" >
-            <img src={logo} alt="my personal logo"  className="w-18 h-18 rounded-full" />
+        bg-center bg-no-repeat  mt-16 px-6 py-6 font-poppins text-gray-200 flex flex-col md:flex-row
+        md:items-center  md:justify-between md:gap-0 gap-4" >
+            <img src={logo} alt="my logo"  className="w-16 h-16 rounded-full" />
             <p className="text-sm  md:text-xl sm:text-lg text-center md:text-right">
                 &copy; {new Date().getFullYear()} <em>Tanko Fabrice. All rights reserved.</em>
              </p>
-            <div className="flex text-white  gap-2">
+            <div className="flex justify-center md:justify-start / md:justify-end text-gray-400  gap-4 md:gap-6">
                 <a href="https://www.facebook.com/share/17gMMJNHhP/" target="_blank" rel="noopener nooreferrer">
                     <Facebook size={36}  />
                 </a>
                 <a href="https://x.com/tanko_fabrice" target="_blank" rel="noopener noreferrer">
-                    <Twitter size={35}  />
+                    <img src={icon} alt="x(twitter) icon" className="w-10 h-10 rounded-full" />
                 </a>
                 <a href="https://github.com/Tanko99/" target="_blank" rel="noopener noreferrer">
                     <Github size={35}  />
