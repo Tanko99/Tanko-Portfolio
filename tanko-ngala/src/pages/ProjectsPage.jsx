e80a9cf00db9ddc814e5ec039b532a8a43b68e66
Tanko Ngala <tankofabrice4@gmail.com>
2025-11-01 16:55:19 +0100

    projects page: toggle theme added

--- tanko-ngala/src/pages/ProjectsPage.jsx
+++ tanko-ngala/src/pages/ProjectsPage.jsx
@@ -9,8 +9,8 @@ function ProjectsPage(){
     }, []);
 
     return (
-        <div className="flex flex-col bg-black font-poppins min-h-screen text-gray-200">
-           <h1 className="lg:text-7xl md:text-5xl sm:text-5xl text-xl mb-8 
+        <div className="flex flex-col bg-white dark:bg-gray-900 dark:text-gray-400  font-poppins min-h-screen text-gray-900">
+           <h1 className="lg:text-7xl md:text-5xl sm:text-5xl text-xl my-6 
             font-bold text-center">My Projects</h1>
            <div className="
             grid grid-col md:grid-cols-3 sm:grid-cols-2 px-6 py-12">
