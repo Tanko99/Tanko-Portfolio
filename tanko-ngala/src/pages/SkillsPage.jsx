bb8cdef339a98cabe25906e1b5448e6ceadca856
Tanko Ngala <tankofabrice4@gmail.com>
2025-11-01 15:00:38 +0100

    skilss page edited for dark mode compatibility

--- tanko-ngala/src/pages/SkillsPage.jsx
+++ tanko-ngala/src/pages/SkillsPage.jsx
@@ -5,16 +5,16 @@ function Skills() {
     <div
       className="bg-white min-h-screen text-gray-900 dark:bg-gray-900  dark:text-gray-200 font-poppins py-16 px-6 md:px-12"
     >
-      <h2 className="sm:text-3xl md:text-5xl lg:text-7xl  font-bold text-center  mb-10">
+      <h2 className="text-xl m:text-3xl md:text-5xl lg:text-7xl  font-bold text-center  mb-10">
         Skills & Tools
       </h2>
 
-      <div className="grid sm:grid-col-1 lg:grid-cols-2 gap-15 max-w-6xl mx-auto">
+      <div className="grid sm:grid-col-1 lg:grid-cols-2 gap-10 mx-auto">
         {/* Front-End Development */}
-        <div className="bg-green-800 rounded-2xl p-6 shadow-md hover:shadow-xl transition-all duration-300">
+        <div className="bg-gray-900 dark:bg-gray-700 dark:text-gray-900  rounded-2xl p-6 shadow-md hover:shadow-xl transition-all duration-300">
           <div className="flex items-center gap-3 mb-4">
             <Code2 className="text-gray-200  w-15 h-15" />
-            <h3 className=" font-semibold md:text-3xl sm:text-xl text-white">
+            <h3 className=" font-semibold md:text-3xl sm:text-xl text-lg text-gray-200">
               Front-End Development
             </h3>
           </div>
@@ -28,10 +28,10 @@ function Skills() {
         </div>
 
         {/* Tools & Workflow */}
-        <div className="bg-green-600 rounded-2xl p-6 shadow-md hover:shadow-xl transition-all duration-300">
+        <div className="bg-gray-900 dark:bg-gray-700 dark:text-gray-900 rounded-2xl p-6 shadow-md hover:shadow-xl transition-all duration-300">
           <div className="flex items-center gap-3 mb-4">
             <Settings className="text-white w-15 h-15" />
-            <h3 className="md:text-3xl sm:text-2xl  font-semibold text-white">
+            <h3 className="md:text-3xl sm:text-2xl  font-semibold text-xl text-gray-200">
               Tools & Workflow
             </h3>
           </div>
@@ -45,7 +45,7 @@ function Skills() {
         </div>
       </div>
 
-      <p className=" md:text-xl sm:text-lg text-base text-center text-gray-200 mt-12 text-sm">
+      <p className=" md:text-xl sm:text-lg text-base text-center dark:text-gray-200 mt-12 text-sm">
         ⚡ Continuously expanding my knowledge in modern web technologies.
       </p>
     </div>
